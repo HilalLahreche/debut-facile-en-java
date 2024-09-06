25536c7770ed6d9ba6d485c6dc45b4c77288ef9f
HilalLahreche <hilallahreche@gmail.com>
2024-09-06 14:32:23 +0200

    p1c6 ok

--- src/com/zerofiltre/parkingbot/ParkingBot.java
+++ src/com/zerofiltre/parkingbot/ParkingBot.java
@@ -1,13 +1,11 @@
 package com.zerofiltre.parkingbot;
 
-import java.security.SecureRandom;
+import com.zerofiltre.parkingbot.model.Ticket;
+import com.zerofiltre.parkingbot.model.Vehicle;
+import com.zerofiltre.parkingbot.service.ParkingService;
 
 public class ParkingBot {
 
-    static String[] customers =
-            {"Leye", "Ali", "Réné", "Booba", "Léa", "Hamza", "Philippe", "Zineb", "Jules", "Mukul", "Will"};
-
-    static int parkingSize = new SecureRandom().nextInt(10);
 
     /**
      * Ceci est la méthode Main
@@ -15,36 +13,17 @@ public class ParkingBot {
      * @param args : Tableau de données entrées lors du lancement de l'application
      */
     public static void main(String[] args) {
-        /*for (int i = 0; i < customers.length; i++) {
-            sayHello(customers[i]);
-        }*/
-
-        /*for(String customer : customers) {
-            sayHello(customer);
-        }*/
-        System.out.println("Nous avons " + parkingSize + "place(s) disponible(s)");
-        int customerNumber =0;
-         while (parkingSize > 0) {
-         parkingSize--;
-
-         if("Réné".equals(customers[customerNumber])) {
-             customerNumber++;
-             parkingSize++;
-             continue;
-         }
-         if("Booba".equals(customers[customerNumber])) {
-             break;
-         }
-         sayHello(customers[customerNumber]);
-        customerNumber++;}
+        processVehicles();
     }
-    /**
-     *
-     * @param customer Le client à qui dire Hello
-     */
-    private static void sayHello(String customer) {
-        String welcomeSentence = "Hello " + customer;
-        System.out.println(welcomeSentence);
+
+    private static void processVehicles() {
+        Vehicle vehicle = new Vehicle();
+        vehicle.setRegistrationNumber("LS-324-PM");
+        vehicle.setCategory("CITADINE");
+        ParkingService parkingService = new ParkingService();
+        Ticket ticket = parkingService.processIncomingVehicle(vehicle);
+        System.out.println(ticket);
     }
+
 
 }
