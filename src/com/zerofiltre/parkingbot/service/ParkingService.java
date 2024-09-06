bb038e51c0e86e06e3f0a8e03c840b3ea130e04d
HilalLahreche <hilallahreche@gmail.com>
2024-09-06 10:48:43 +0200

    p1c5 Ok

--- src/com/zerofiltre/parkingbot/service/ParkingService.java
+++
@@ -1,7 +0,0 @@
-package com.zerofiltre.parkingbot.service;
-
-public class ParkingService {
-   public String parking = "Parking et surveillance de vehicules";
-    public String washing = " Lavage de voitures";
-
-}
